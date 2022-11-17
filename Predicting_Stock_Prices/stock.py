ada9b1e0d926b1ba31430f719dd8f3bdbaa75076
Sachin Khargie <sachx1281@gmail.com>
2022-11-16 23:51:47 -0500

    changed data set to 30 days

--- Predicting_Stock_Prices/stock.py
+++ Predicting_Stock_Prices/stock.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import csv
 from sklearn.svm import SVR
 
@@ -16,19 +17,19 @@ def get_data(filename):
     return
 
 def predict_prices(dates, prices, x):
-    dates = np.reshape(dates,(len(dates), 1))
+    dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
 
-    svr_lin = SVR(kernel= 'linear', C=1e3)
-    svr_poly = SVR(kernel = 'poly', C=1e3, degree=2)
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    svr_lin = SVR(kernel= 'linear', C= 1e3)
+    svr_poly = SVR(kernel= 'poly', C= 1e3, degree= 2)
+    svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
+    svr_rbf.fit(dates, prices) # fitting the data points in the models
     svr_lin.fit(dates, prices)
     svr_poly.fit(dates, prices)
-    svr_rbf.fit(dates, prices)
 
-    plt.scatter(dates, prices, color='black', label='Data')
-    plt.plot(dates, svr_rbf.predict(dates), color='red', label='RBF model')
-    plt.plot(dates, svr_lin.predict(dates), color='green', label='Linear Model')
-    plt.plot(dates, svr_poly.predict(dates), color='blue', label='Polynomial Model')
+    plt.scatter(dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints 
+    plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model') # plotting the line made by the RBF kernel
+    plt.plot(dates,svr_lin.predict(dates), color= 'green', label= 'Linear model') # plotting the line made by linear kernel
+    plt.plot(dates,svr_poly.predict(dates), color= 'blue', label= 'Polynomial model') # plotting the line made by polynomial kernel
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('Support Vector Regression')
@@ -37,12 +38,9 @@ def predict_prices(dates, prices, x):
 
     return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-get_data('TSLA.csv')
+get_data('TSLAV2.csv')
 
 predicted_price = predict_prices(dates, prices, 29)
-
-print(predicted_price)
-
 
 # OLD CODE
 # # load data
